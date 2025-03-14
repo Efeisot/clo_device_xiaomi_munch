529521fd2dbceae90fde139d3c883722e3a44382
isot <efeisotkmr@gmail.com>
2025-03-14 17:44:05 +0300

    clo_mk: updated vendor fingerprint

--- arrow_munch.mk
+++ arrow_munch.mk
@@ -24,4 +24,6 @@ PRODUCT_MODEL := POCO F4
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V14.0.3.0.TLMMIXM:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     BuildDesc="munch-user 14 UKQ1.231207.002 V816.0.8.0.ULMMIXM release-keys" \
+     BuildFingerprint=Redmi/munch/munch:14/UKQ1.231207.002/V816.0.8.0.ULMMIXM:user/release-keys
